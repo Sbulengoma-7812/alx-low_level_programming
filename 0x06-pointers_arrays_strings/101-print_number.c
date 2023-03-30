1035ce54a7cdfc79f4ab8bd797b63c8e5a6bfd00
Sibongile Simbeye <sibongilesimbeyejy@gmail.com>
2023-03-30 10:39:22 -0700

    Numbers have meaning

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,20 +4,19 @@
  * print_number - prints an integer;
  * @n: integer to be printed;
  */
-
 void print_number(int n)
 {
+	unsigned int n1;
 
-	unsigned int num = n1;
-
-	if (n1 < 0)
+	if (n < 0)
 	{
-		num = -num;
-		_putchar('-')
+		n1 = -n;
+		_putchar('-');
+	} else
+	{
+		n1 = n;
 	}
-
-	if ((num / 10) > 0)
-		print_number(num / 10);
-
-	_putchar((num % 10) + '0');
+	if (n1 / 10)
+		print_number(n1 / 10);
+	_putchar((n1 % 10) + '0');
 }
